47acfbd5aafae567bca1c0905caebbc7af657de9
Xiang Zhu <zhuxiang19881117@gmail.com>
2016-04-17 22:45:23 -0700

    To solve the problem by sort and comparison
    low efficiency

--- src/242_valid_anagram.py
+++ src/242_valid_anagram.py
@@ -5,31 +5,18 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        flag = False
-        s_dic = self.turn2dic(s)
-        t_dic = self.turn2dic(t)
-        # print s_dic
-        # print t_dic
-        if len(s_dic)==len(t_dic):
-            dic_equal = True
-            for key in s_dic.keys():
-                if not t_dic.has_key(key):
-                    dic_equal = False
-                    break
-                elif s_dic[key] != t_dic[key]:
-                    dic_equal = False
-                    break
-            flag = dic_equal
-        return flag
-
-    def turn2dic(self, s):
-        s_dic = {}
-        for char in s:
-            if s_dic.has_key(char):
-                s_dic[char] += 1
-            else:
-                s_dic[char] = 1
-        return s_dic
+        if s==None and t==None:
+            return True
+        elif s==None or t==None:
+            return False
+        elif len(s) != len(t):
+            return False
+        else:
+            s = sorted(s)
+            t = sorted(t)
+            for (x,y) in zip(s,t):
+                if x != y: return False
+            return True
 
 if __name__ == '__main__':
     s = "anagram"

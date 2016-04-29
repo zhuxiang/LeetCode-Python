6437cba90231be8e0f838944337f7f67f9555898
Xiang Zhu <zhuxiang19881117@gmail.com>
2016-04-29 00:12:45 -0700

    Stefen's algorithm

---
+++ src/234-PalindromeLinkedList.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head or not head.next:
+            return True
+        fast = slow = head
+        rev = None
+        # Find the middle element and get the reverse list from middle to head
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+            slow = slow.next
+        while rev and rev.val == slow.val:
+            rev = rev.next
+            slow = slow.next
+        return not rev
+
+if __name__ == '__main__':
+    l1 = ListNode(1)
+    l2 = ListNode(2)
+    l1.next = l2
+    

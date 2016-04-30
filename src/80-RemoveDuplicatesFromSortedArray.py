d1485260f2f03cc92d349dcbf53c0f330942738e
Xiang Zhu <zhuxiang19881117@gmail.com>
2016-04-29 22:23:27 -0700

    Your runtime beats 63.35% of pythonsubmissions.

--- src/80-RemoveDuplicatesFromSortedArray.py
+++ src/80-RemoveDuplicatesFromSortedArray.py
@@ -4,26 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n = len(nums)
-        if n < 3:
-            return n
-        i = 1
-        cntSame = 1
-        while i < n:
-            if nums[i] == nums[i-1]:
-                cntSame += 1
-                if cntSame > 2:
-                    nums.remove(nums[i])
-                    i -= 1
-                    n -= 1
-            else:
-                cntSame = 1
-            i += 1
-        return n
+        i = 0
+        for n in nums:
+            if i < 2 or n > nums[i-2]:
+                nums[i] = n
+                i += 1
+        return i
 
 
 if __name__ == '__main__':
     nums = [1,1,1,2,2,2,2,2,3,3]
     s = Solution()
-    s.removeDuplicates(nums)
-    print nums
+    i = s.removeDuplicates(nums)
+    print nums[0:i]

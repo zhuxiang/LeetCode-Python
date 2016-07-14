c93c683b0c89413d3c5eb11844566a10e9ad463c
Xiang <juneshy1117@foxmail.com>
2016-07-14 12:15:22 +0800

    nothing change

--- src/3-LengthOfLongestSubstring.py
+++ src/3-LengthOfLongestSubstring.py
@@ -6,13 +6,10 @@ class Solution(object):
         """
         start = maxLength = 0
         usedChar = {}
-
         for i in range(len(s)):
             if s[i] in usedChar and start <= usedChar[s[i]]:
                 start = usedChar[s[i]] + 1
             else:
                 maxLength = max(maxLength, i - start + 1)
-
             usedChar[s[i]] = i
-
         return maxLength

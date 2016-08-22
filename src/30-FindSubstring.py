e2f2455de4c8f655caa3666694e3aa0ee9a4d8a8
Xiang Zhu <zhuxiang19881117@gmail.com>
2016-08-22 20:58:35 +0800

    hash table

--- src/30-FindSubstring.py
+++ src/30-FindSubstring.py
@@ -1,3 +1,5 @@
+import copy
+
 class Solution(object):
     def findSubstring(self, s, words):
         """
@@ -5,35 +7,59 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[int]
         """
+        wordLen = self.calWordLen(words)
+        wordDict = self.genWordDict(words)
+        n = wordLen * len(words)
+        indexList = []
+        for i in xrange(wordLen):
+            start = i
+            chkDict = {}
+            cnt = 0
+            for j in xrange(i, len(s) - wordLen + 1, wordLen):
+                word = s[j:j+wordLen]
+                if word in wordDict:
+                    if word in chkDict:
+                        chkDict[word] += 1
+                    else:
+                        chkDict[word] = 1
+                    cnt += 1
+                    while chkDict[word] > wordDict[word]:
+                        chkDict[s[start:start+wordLen]] -= 1
+                        start += wordLen
+                        cnt -= 1
+                    if cnt == len(words):
+                        indexList.append(start)
+                else:
+                    chkDict = {}
+                    start = j + wordLen
+                    cnt = 0
+        return indexList
 
-    def generateSubstrings(this, words):
+    def calWordLen(self, words):
         """
         :type words: List[str]
-        :rtype substrings: List[str]
+        :rtype: int
         """
-        if len(words) == 1:
-            yield words[0]
+        if len(words) == 0:
+            return 0
         else:
-            for substring in self.generateSubstrings(words[:-1]):
-                
+            return len(words[0])
 
-    def makeNext(this, pat):
+    def genWordDict(self, words):
         """
-        :type pat: str
-        :rtype next: List[int]
+        :type words: List[str]
+        :rtype: Dict[int]
         """
-        # q: the index of pat
-        # k: the length of maximum prefix and suffix
-        q, k = 1, 0
-        # m: the length of pat
-        m = len(pat)
-        # next: partial match dict for KMP algorithm
-        next = [0] * m
-        while q < m:
-            if k > 0 and pat[q] != pat[k]:
-                k = next[k - 1]
-            if pat[q] == pat[k]:
-                k += 1
-            next[q] = k
-            q += 1
-        return next
+        wordDict = {}
+        for word in words:
+            if wordDict.has_key(word):
+                wordDict[word] += 1
+            else:
+                wordDict[word] = 1
+        return wordDict
+
+if __name__ == '__main__':
+    sln = Solution()
+    s = "wordgoodgoodgoodbestword"
+    words = ["word","good","best","good"]
+    print sln.findSubstring(s, words)
